03d366b0565997ca5ed3acf07f0f7eeb4c20a636
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-07-07 18:08:58 +0530

    Update Soundex.Tests.cpp
    add test case

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -6,4 +6,6 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   char soundex[5];
   generateSoundex("AX", soundex);
   ASSERT_EQ(soundex,"A200");
+  generateSoundex("Acraft", soundex);
+  ASSERT_EQ(soundex, "A261");
 }
