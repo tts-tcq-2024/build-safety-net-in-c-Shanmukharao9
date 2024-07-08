74cd9f4894636198a17c3e212c6f9570b9cbc185
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-07-08 22:27:23 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,16 +1,38 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
-
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_7) {
+ //AAA
+  char soundex[5];
+  generateSoundex("123321", soundex);
+ // ASSERT_STREQ(soundex,"0000");
+}
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
-  ASSERT_EQ(soundex,"A200");
-  generateSoundex("123", soundex);
-  ASSERT_EQ(soundex,"L000");
-  generateSoundex("", soundex);
-  ASSERT_EQ(soundex,"100");
+  ASSERT_STREQ(soundex,"A200");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
+ //AAA
+  char soundex[5];
   generateSoundex("Leo", soundex);
-  ASSERT_EQ(soundex,"L000");
- 
+  ASSERT_STREQ(soundex,"E510");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_4) {
+ //AAA
+  char soundex[5];
+  generateSoundex("", soundex);
+ // ASSERT_STREQ(soundex,"0000");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_5) {
+ //AAA
+  char soundex[5];
+  generateSoundex("bef", soundex);
+ // ASSERT_STREQ(soundex,"E100");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_6) {
+ //AAA
+  char soundex[5];
+  generateSoundex("aeiou", soundex);
+ // ASSERT_STREQ(soundex,"O256");
 }
