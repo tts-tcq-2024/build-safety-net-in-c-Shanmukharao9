c48eb0bab4c43b6e5241e5aa35016a82e30b9c97
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-04 21:59:28 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,13 +5,13 @@ char soundex[5];
 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //Basic tests
-    testSoundex("Example", soundex);
-    testSoundex("Euler", soundex);
-    testSoundex("Gauss", soundex);
-    testSoundex("Hilbert", soundex);
-    testSoundex("Knuth", soundex);
-    testSoundex("Lloyd", soundex);
-    testSoundex("Lukasiewicz", soundex);
+    generateSoundex("Example", soundex);
+    generateSoundex("Euler", soundex);
+    generateSoundex("Gauss", soundex);
+    generateSoundex("Hilbert", soundex);
+    generateSoundex("Knuth", soundex);
+    generateSoundex("Lloyd", soundex);
+    generateSoundex("Lukasiewicz", soundex);
 
   //ASSERT_EQ(soundex,"R163");
 }
